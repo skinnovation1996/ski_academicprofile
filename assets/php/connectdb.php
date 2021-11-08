d86c8f140bb97ed9da536aac3b23b6f6ea09807b
skinnovation1996 <92845186+skinnovation1996@users.noreply.github.com>
2021-11-08 10:14:50 +0800

    comment out azure DB connection

--- assets/php/connectdb.php
+++ assets/php/connectdb.php
@@ -1,15 +1,17 @@
 <?php
-	$servername = "ski-academicprofiledbf.mysql.database.azure.com";
-	$username = "skiadmindb";
-	$password = 'Pk@s12345';
+	$servername = "localhost";
+	$username = "root";
+	$password = '';
 	$dbname = "ski_academicprofile";
 
-    $conn = mysqli_init();
-    mysqli_ssl_set($conn,NULL,NULL, "assets/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-    mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+	$conn = mysqli_connect($servername, $username, $password, $dbname)or die(mysqli_connect_error());
 
-    if (mysqli_connect_errno($conn)) {
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
-    }
+//     $conn = mysqli_init();
+//     mysqli_ssl_set($conn,NULL,NULL, "assets/certs/DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+//     mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+
+//     if (mysqli_connect_errno($conn)) {
+//         die('Failed to connect to MySQL: '.mysqli_connect_error());
+//     }
 ?>
   
